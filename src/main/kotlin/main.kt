c532d5c2ab8d10548f0f71fd8bb89461980f83b6
AntonKuznetsov2022 <anton_kuznetsov@bk.ru>
2022-07-28 18:11:47 +0300

    homework-1-1

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,8 @@
 fun main () {
-    println("1")
+    val transferAmount: Double = 4670.50
+    val commission: Double = 0.0075
+    var ammount: Double =0.0
+
+    ammount = transferAmount * commission
+    if (ammount<35) println("Комиссия равна 35 руб.") else println("Комиcсия равна: ${String.format("%.2f", ammount)} руб.")
 }
